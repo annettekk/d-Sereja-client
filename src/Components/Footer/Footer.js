1a4b8de71952cac3a1fe24eef18f886b9707e1af
Yaga Gauganek <agataeblumkowska@gmail.com>
2023-08-07 16:08:33 +0100

    footer update

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,9 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <>
-      <div>Footer</div>
+      <div>
+        <h4>Jaga Galganek Inc.</h4>
+      </div>
     </>
   );
 }

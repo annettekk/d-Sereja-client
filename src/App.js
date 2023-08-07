c9dad836024875f9eb3ef7e9c1dfcbfc9c191836
BILLIEBAXTER <hello.billiebaxter@gmail.com>
2023-08-07 14:42:46 +0100

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header></Header>
+      <h2>App</h2>
     </div>
   );
 }

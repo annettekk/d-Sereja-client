21f686297ccebca34cb6dbf836ac75f2cba5edca
Anna <akuroshenkova@gmail.com>
2023-08-08 15:23:10 +0100

    cartmodal added

--- src/Components/CartModal/CartModal.js
+++ src/Components/CartModal/CartModal.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./CartModal.css";
 
-export default function CartModal() {
-  return(  
-    <>
-      <div>CartModal</div>
-    </>
-  );
+export default function CartModal({ closeCartModal }) {
+  return (
+    <div className="cartItems closed">
+        <div className="itemContainer">
+        Your Cart
+        <span className="cross" onClick={closeCartModal}>
+          X
+        </span>
+
+        </div>
+
+    </div>
+  )
 }

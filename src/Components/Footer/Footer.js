cb822ebd2ba9479c1cf1cc9a4b8904ed73e59d43
Yaga Gauganek <agataeblumkowska@gmail.com>
2023-08-08 10:29:01 +0100

    footer icons add

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./Footer.css";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaBehanceSquare } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <>
       <div>
         <h3>Jaga Galganek Inc.</h3>
-        <a href={`https://www.behance.net/bakajacagaa403`}></a>
-        <a href={`https://www.instagram.com/jagagalganek/?hl=en`}></a>
+        <a href={`https://www.behance.net/bakajacagaa403`}>
+          <FaBehanceSquare />
+        </a>
+        <a href={`https://www.instagram.com/jagagalganek/?hl=en`}>
+          <AiFillInstagram />
+        </a>
       </div>
     </>
   );

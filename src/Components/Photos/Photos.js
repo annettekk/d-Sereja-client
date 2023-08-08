92924326b9eab030fa9d6e500b342454b2d65d7e
BILLIEBAXTER <hello.billiebaxter@gmail.com>
2023-08-08 16:37:15 +0100

    aaa

--- src/Components/Photos/Photos.js
+++ src/Components/Photos/Photos.js
@@ -1,55 +1,55 @@
-import React, { useEffect, useState } from 'react'
-import "./Photos.css"
-import axios from "axios"
+import React, { useEffect, useState } from "react";
+import "./Photos.css";
+import axios from "axios";
 import { FcLike } from "react-icons/fc";
 import { BsFillCartPlusFill } from "react-icons/bs";
 
+export default function Photos({ addToCart }) {
+  const [photos, setPhotos] = useState([]);
 
+  useEffect(() => {
+    getPhotos();
+  });
 
-import "../.././"
-
-export default function Photos() {
-    const[photos, setPhotos] = useState([])
-    
-    useEffect(() => {
-        getPhotos()
-    })
-
-    async function getPhotos(){
-        try{
-            let API = 'http://localhost:9080/photos'
-            const result = await axios.get(API)
-            setPhotos([])
-            setPhotos(result.data)
-        }catch(error){
-            console.log(error)
-        }
+  async function getPhotos() {
+    try {
+      let API = "http://localhost:9080/photos";
+      const result = await axios.get(API);
+      setPhotos([]);
+      setPhotos(result.data);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    function likePhoto(photo){
-        console.log(`photo with id ${photo._id} has been liked`)
-    }
+  function likePhoto(photo) {
+    console.log(`photo with id ${photo._id} has been liked`);
+  }
 
-    function addToCart(photo){
-        console.log(`Photo ${photo.title} has been added to cart`)
-    }
-
-    function handlePhotoModal(photo){
-        console.log(`displaying larger photo of ${photo.title}`)
-        
-    }
+  function handlePhotoModal(photo) {
+    console.log(`displaying larger photo of ${photo.title}`);
+  }
 
   return (
     <>
-        <div>Photos</div>
-        {photos.map((photo, _id) => (
-            <div key={_id} className='photoDiv'>
-                <h3 className=''>{photo.title}</h3>
-                <img className='photo' src={require(`../../${photo.photoSrc}`)} alt={photo.title} onClick={() => handlePhotoModal(photo)}></img>
-                <button onClick={() => likePhoto(photo)}><FcLike /></button>
-                <button onClick={() => addToCart(photo)}><BsFillCartPlusFill/></button>
-            </div>
-        ))}
+      <div>Photos</div>
+      {photos.map((photo, _id) => (
+        <div key={_id} className="photoDiv">
+          <h3 className="">{photo.title}</h3>
+          <img
+            className="photo"
+            src={require(`../../${photo.photoSrc}`)}
+            alt={photo.title}
+            onClick={() => handlePhotoModal(photo)}
+          ></img>
+          <button onClick={() => likePhoto(photo)}>
+            <FcLike />
+          </button>
+          <button onClick={() => addToCart(photo)}>
+            <BsFillCartPlusFill />
+          </button>
+        </div>
+      ))}
     </>
-  )
+  );
 }

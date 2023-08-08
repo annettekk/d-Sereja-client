b0dc804a4766f2fa4c2a167b9cb0397cd0a29b9f
Yaga Gauganek <agataeblumkowska@gmail.com>
2023-08-08 14:11:52 +0100

    about content update, no css

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./About.css";
 import { Helmet } from "react-helmet-async";
+import aboutPhoto from "../../Images/aboutPhoto.jpg";
+import { Link } from "react-router-dom";
 
 export default function About() {
   return (
@@ -15,16 +17,59 @@ export default function About() {
       </Helmet>
       <div>
         <h2 className="about-header">About</h2>
+        <img className="about-photo" src={aboutPhoto} alt="About photo" />
         <p>
-          Polish film photographer, based in Suffolk, UK. She shoots film since
-          her teenage years. Her first camera was soviet Zenit-E. At the moment
-          she uses 35mm - Pentax ME super and medium format - Pentax 6x7. Her
-          interest is focused around portrait where she can express herself.
-          Main motif of her photography is expression of melancholy. Models on
-          her photos are strong and vulnerable at the same of time. Yaga is
-          experimenting with double exposures as well as with cyanotype print
-          techniques in her work.
+          Jaga is a Polish film photographer, based in Suffolk, UK. She shoots
+          film since her teenage years. Her first camera was soviet Zenit-E. At
+          the moment she uses 35mm - Pentax ME super and medium format - Pentax
+          6x7. She perceives her photography as a rich tapestry of
+          contradictions and ambivalence. A central objective for her is to
+          seamlessly integrate her feminine and feministic perspective into her
+          work, embracing the complexity inherent in both. While her models may
+          exude an apparent fragility and vulnerability, it is their profound
+          sensitivity that becomes their ultimate strength. Their power and
+          confidence emanate from a beauty that transcends mere physical
+          appearance, instead rooted in the depth of heir personal experiences.
+          Additionally, the portraits she captures serve as a profound
+          reflection of her own inner self, from times when she suffered
+          depression several years ago. Echoes of that sombreness can be
+          discerned within her portraits - a touch of ethereality, an evocation
+          of suspended realms between dreams and reality. Occasionally, even a
+          subtle element may emerge, subtly unsettling the viewer, perhaps even
+          evoking a sense of foreboding.
         </p>
+        <h2 className="equipment">Equipment</h2>
+        <p>
+          Navigating the realms of film, Jaga wields the Pentax ME Super and
+          Pentax 6x7 as her creative companions. With the ME Super, she dances
+          between hues, capturing life in vivid colors while honing her focus
+          with its seamless blend of manual finesse and automatic ease. The 6x7
+          elevates her art further, embracing medium format grandeur for
+          unmatched clarity and range. Through this gear, grain becomes her
+          symphony, adding depth to each frame. In a digital age, her devotion
+          to film crafts timeless tales, each click an ode to enduring artistry
+        </p>
+        <h3>Featured in:</h3>
+        <ul>
+          <li>
+            <Link to="https://www.vogue.com/photovogue/photographers/207945">
+              Vogue
+            </Link>
+          </li>
+          <li>
+            <Link to="https://www.lomography.com/magazine/">
+              Lomography Interview
+            </Link>
+          </li>
+          <li>
+            <Link to="https://www.instagram.com/international.film.swap.group/?hl=en">
+              International Film Swap
+            </Link>
+          </li>
+        </ul>
+
+        <h2 className="contact">Contact</h2>
+        <p className="email">jagagalganek@gmail.com</p>
       </div>
     </>
   );

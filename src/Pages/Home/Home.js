af23ef1683fa335fdf99369b01c75446b728a691
Yaga Gauganek <agataeblumkowska@gmail.com>
2023-08-08 11:29:16 +0100

    hero image

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,20 +1,23 @@
 import React from "react";
 import "./Home.css";
 import { Helmet } from "react-helmet-async";
+// import Hero-home.jpg from "./Hero-home";
 
 export default function Home() {
   return (
     <>
-    <Helmet>
-      <title>Jaga Galganek Photography Home Page</title>
-      <meta
-      name="description"
-      content="This is the home page for Jaga Galganek's Photography Portfoilio"/>
-      <link rel="canonical" href="/"/>
-    </Helmet>
+      <Helmet>
+        <title>Jaga Galganek Photography Home Page</title>
+        <meta
+          name="description"
+          content="This is the home page for Jaga Galganek's Photography Portfoilio"
+        />
+        <link rel="canonical" href="/" />
+      </Helmet>
       <main>
         <h2>Home Page</h2>
+        <img className="hero-image" src={Hero - home.jpg} />
       </main>
     </>
-  )
+  );
 }

cb64578536705d6d3d9f4d45fad17fbaec9b3bcd
BILLIEBAXTER <hello.billiebaxter@gmail.com>
2023-08-08 15:40:29 +0100

    buttons

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,25 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiShoppingCart } from "react-icons/fi";
 
-
-export default function Header({handleCartModal}) {
-  
+export default function Header({ handleCartModal }) {
   return (
-    
     <header>
-        <h1>Jaga Galganek Photography</h1>
-        <nav>
-          <ul>
-            <li className='navLi'> <Link className='link' to={"/"}>Home</Link></li>
-            <li className='navLi'> <Link className='link' to={"/about"}>About</Link></li>
-            <li className='navLi'> <Link className='link' to={"/gallery"}>Gallery</Link></li>
-            <li className='navLi'> <Link className='link' to={"/contact"}>Contact</Link></li>
-
-          </ul>
-        </nav>
-        <div>Login placeholder</div>
-        <button onClick={() => handleCartModal()}></button>
-        
-      </header>
-  )
+      <h1>Jaga Galganek Photography</h1>
+      <nav>
+        <ul>
+          <li className="navLi">
+            {" "}
+            <Link className="link" to={"/"}>
+              Home
+            </Link>
+          </li>
+          <li className="navLi">
+            {" "}
+            <Link className="link" to={"/about"}>
+              About
+            </Link>
+          </li>
+          <li className="navLi">
+            {" "}
+            <Link className="link" to={"/gallery"}>
+              Gallery
+            </Link>
+          </li>
+          <li className="navLi">
+            {" "}
+            <Link className="link" to={"/contact"}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div>Login placeholder</div>
+      <button onClick={() => handleCartModal()}>
+        <FiShoppingCart />
+      </button>
+    </header>
+  );
 }

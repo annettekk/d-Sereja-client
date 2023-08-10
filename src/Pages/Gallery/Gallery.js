e958d3d2a54bd991520ff81be7a7f50573618e7f
JessicaBane <jessicambane@gmail.com>
2023-08-10 18:51:56 +0100

    fixed cart overlay for home page, added hover and click for buttons and styled them better

--- src/Pages/Gallery/Gallery.js
+++ src/Pages/Gallery/Gallery.js
@@ -31,7 +31,6 @@ export default function Gallery({ addToCart }) {
         <link rel="canonical" href="/gallery" />
       </Helmet>
       <main>
-        <h2 className="galleryHeader">Gallery</h2>
         <Photos addToCart={addToCart} handlePhotoModal={handlePhotoModal}/>
         {photoModal && <PhotoModal photoModalContent={photoModalContent} closePhotoModal={closePhotoModal}/>} 
       </main>

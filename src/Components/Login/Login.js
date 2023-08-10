2e84e5b801d19acce235172afb39ea6e3720924e
Yaga Gauganek <agataeblumkowska@gmail.com>
2023-08-10 10:43:20 +0100

    login/log icon add

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,10 +1,11 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
+import { FiLogIn } from "react-icons/fi";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
-
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <FiLogIn onClick={() => loginWithRedirect()} />;
+  // return <button onClick={() => loginWithRedirect()}>Log In</button>;
 };
 
 export default LoginButton;
--- src/Components/Logout/Logout.js
+++ src/Components/Logout/Logout.js
@@ -1,17 +1,24 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
+import { FiLogOut } from "react-icons/fi";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button
+    <FiLogOut
       onClick={() =>
         logout({ logoutParams: { returnTo: window.location.origin } })
       }
-    >
-      Log Out
-    </button>
+    />
+
+    // <button
+    //   onClick={() =>
+    //     logout({ logoutParams: { returnTo: window.location.origin } })
+    //   }
+    // >
+    //   Log Out
+    // </button>
   );
 };
 

1d574a9cc03e015bec47691f13175623ad5434ef
Yaga Gauganek <agataeblumkowska@gmail.com>
2023-08-10 17:05:24 +0100

    footer update

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,18 +2,26 @@ import React from "react";
 import "./Footer.css";
 import { AiFillInstagram } from "react-icons/ai";
 import { FaBehanceSquare } from "react-icons/fa";
+import { GiHealthPotion } from "react-icons/gi";
 
 export default function Footer() {
   return (
     <>
-      <footer>
-        <h4>Jaga Galganek Inc.</h4>
-        <a href={`https://www.behance.net/bakajacagaa403`}>
-          <FaBehanceSquare />
-        </a>
-        <a href={`https://www.instagram.com/jagagalganek/?hl=en`}>
-          <AiFillInstagram />
-        </a>
+      <footer className="footer">
+        <h5 className="footerName">
+          Made with <GiHealthPotion /> by: Baba-Yaga Coding Team
+        </h5>
+        <div className="icons">
+          <a className="bh" href={`https://www.behance.net/bakajacagaa403`}>
+            <FaBehanceSquare />
+          </a>
+          <a
+            className="ins"
+            href={`https://www.instagram.com/jagagalganek/?hl=en`}
+          >
+            <AiFillInstagram />
+          </a>
+        </div>
       </footer>
     </>
   );

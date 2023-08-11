fa70b31ad65c55412ad266192532118768c34160
Yaga Gauganek <agataeblumkowska@gmail.com>
2023-08-11 11:00:24 +0100

    admin add to burger

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./Burger.css";
 import { slide as Menu } from "react-burger-menu";
-
+import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 export default function Burger() {
+  const { user } = useAuth0();
   return (
     <Menu className="my-menu" width={"100vw"} isOpen={false} noOverlay right>
       <a className="menu-item" href="/">
@@ -13,6 +15,17 @@ export default function Burger() {
       </a>
       <a className="menu-item" href="/gallery">
         Gallery
+      </a>
+      <a className="menu-item" href="/admin">
+        {user &&
+          (user.email === process.env.REACT_APP_EMAIL ||
+            user.email === process.env.REACT_APP_EMAIL2 ||
+            user.email === process.env.REACT_APP_EMAIL3 ||
+            user.email === process.env.REACT_APP_EMAIL4) && (
+            <Link className="link" to="/admin">
+              Admin
+            </Link>
+          )}
       </a>
     </Menu>
   );
